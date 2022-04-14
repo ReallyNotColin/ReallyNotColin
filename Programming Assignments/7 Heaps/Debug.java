916519be08630fd5db4f70b0b68ee45ea6d2d2ed
ColinBumpass <57447291+ReallyNotColin@users.noreply.github.com>
2022-04-14 00:47:08 -0500

    Delete Programming Assignments directory

--- Programming Assignments/7 Heaps/Debug.java
+++
@@ -1,9 +0,0 @@
-public class Debug {
-    private static final boolean IS_ON = false;
-
-    public static void print(String msg){
-        if(IS_ON){
-            System.out.println(msg);
-        }
-    }
-}
